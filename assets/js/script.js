57bf91e91c37dfbe81e3d4a832314a2e437ebeff
Greta <50132963+GretaVait@users.noreply.github.com>
2021-02-23 17:32:04 +0200

    updates / fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,11 +45,13 @@
 
 //---- NAVIGATION ----//
 {
+  const body = document.querySelector('body');
   const header = document.querySelector('header');
   const menu = document.querySelector('.menu');
   const menuIcon = document.querySelector('.menu__open');
   const navPrimary = document.querySelector('#nav-primary');
   const navMobile = document.querySelector('#nav-mobile');
+  const navItems = document.querySelectorAll('#nav-mobile a[href^="#"]');
 
   menu.addEventListener('click', () => {
     navPrimary.classList.toggle('active');
@@ -57,6 +59,18 @@
     //
     header.classList.toggle('open');
     navMobile.classList.toggle('open');
+    body.classList.toggle('disable');
+  });
+
+  // console.log(navItem);
+  window.addEventListener('click', function(e){  
+    navItems.forEach(item => {
+      if (item.contains(e.target)){
+        header.classList.remove('open');
+        navMobile.classList.remove('open');
+        body.classList.remove('disable');
+      }
+    }); 
   });
 }
 //---- NAVIGATION ----//
@@ -153,4 +167,197 @@ if (filterBtns.length != 0) {
       }
     });
   });
+}
+
+//---- ANIMATIONS ----//
+const scrollController = new ScrollMagic.Controller();
+const debug = false;
+const triggerHook = 0.8;
+{
+  //
+  header();
+  introAnimations();
+  workAnimations();
+  aboutAnimation();
+  contactAnimation();
+  //
+  function header() {
+    const triggerElement = document.querySelector('header');
+
+    const tl = gsap.timeline();
+    tl.to(triggerElement, {
+      duration: 0, 
+      opacity: 0,
+      y: 20
+    });
+    //--
+    tl.to(triggerElement, {
+      duration: 0.8, 
+      opacity: 1,
+      y: 0
+    }); 
+
+    animate(triggerElement, tl);
+  }
+  function introAnimations() {
+    const triggerElement = document.querySelector('#intro');
+
+    const tl = gsap.timeline();
+    tl.to(triggerElement, {
+      duration: 0, 
+      opacity: 0,
+      y: 20
+    });
+    //--
+    tl.to(triggerElement, {
+      duration: 0.8, 
+      opacity: 1,
+      y: 0
+    }); 
+
+    animate(triggerElement, tl);
+  }
+  function workAnimations() {
+    const triggerElement = document.querySelector('#work');
+    const workToggle = triggerElement.querySelector('.js-work-toggle');
+    const workItem = triggerElement.querySelectorAll('.js-work-item');
+
+    const tl = gsap.timeline();
+    tl.to(workToggle, {
+      duration: 0, 
+      opacity: 0,
+      y: 20
+    });
+    for(let i = 0; i < workItem.length; i++) {
+      tl.to(workItem[i], {
+        duration: 0, 
+        opacity: 0
+      });      
+    }
+    //--
+    tl.to(workToggle, {
+      duration: 0.8, 
+      opacity: 1,
+      y: 0
+    });  
+    for(let i = 0; i < workItem.length; i++) {
+      tl.to(workItem[i], {
+        duration: 0.8, 
+        opacity: 1
+      }, "-=0.7");      
+    }
+
+    animate(triggerElement, tl);
+  }
+  function aboutAnimation() {
+    const triggerElement = document.querySelector('#about');
+    const aboutDescription = triggerElement.querySelector('.js-about-description');
+    const aboutCards = triggerElement.querySelectorAll('.js-about-card');
+
+    const tl = gsap.timeline();
+    tl.to(aboutDescription, {
+      duration: 0, 
+      opacity: 0,
+      y: 20
+    });
+    for(let i = 0; i < aboutCards.length; i++) {
+      tl.to(aboutCards[i], {
+        duration: 0, 
+        opacity: 0,
+        y: 20
+      });      
+    }
+    //--
+    tl.to(aboutDescription, {
+      duration: 0.8, 
+      opacity: 1,
+      y: 0
+    });
+    for(let i = 0; i < aboutCards.length; i++) {
+      tl.to(aboutCards[i], {
+        duration: 0.8, 
+        opacity: 1,
+        y: 0
+      }, "-=0.7");      
+    }
+    
+    animate(triggerElement, tl);
+  }
+  function contactAnimation() {
+    const triggerElement = document.querySelector('#contact');
+    const contactTitle = triggerElement.querySelector('.js-contact-title');
+    const contactItems = triggerElement.querySelectorAll('.js-contact-item');
+    const contactContent = triggerElement.querySelectorAll('.js-contact-content');
+
+    const tl = gsap.timeline();
+    tl.to(contactTitle, {
+      duration: 0, 
+      opacity: 0,
+      y: 20
+    });
+    for(let i = 0; i < contactItems.length; i++) {
+      tl.to(contactItems[i], {
+        duration: 0, 
+        opacity: 0,
+        y: 20
+      });      
+    }
+    tl.to(contactContent, {
+      duration: 0, 
+      opacity: 0,
+      y: 20
+    });
+    //--
+    tl.to(contactTitle, {
+      duration: 0.8, 
+      opacity: 1,
+      y: 0
+    });
+    for(let i = 0; i < contactItems.length; i++) {
+      tl.to(contactItems[i], {
+        duration: 0.8, 
+        opacity: 1,
+        y: 0
+      }, "-=0.7");      
+    }
+    tl.to(contactContent, {
+      duration: 0.8, 
+      opacity: 1,
+      y: 0
+    }, "-=0.7");
+
+    animate(triggerElement, tl);
+  }
+
+}
+
+// Animation functions
+function addClass(triggerElement) {
+  const scene = new ScrollMagic.Scene({
+    reverse: false,
+    triggerHook: triggerHook,
+    triggerElement: triggerElement
+  })
+  .setClassToggle(triggerElement, "revealed")
+  .addTo(scrollController);
+  
+  if(debug)
+  {
+    scene.addIndicators();
+  }
+}
+
+function animate(triggerElement, tl) {
+  const scene = new ScrollMagic.Scene({
+    reverse: false,
+    triggerHook: triggerHook,
+    triggerElement: triggerElement
+  })
+  .setTween(tl)
+  .addTo(scrollController);
+  
+  if(debug)
+  {
+    scene.addIndicators();
+  }
 }
